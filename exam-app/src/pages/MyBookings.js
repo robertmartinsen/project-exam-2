0a354721a80c7743a3bb6a38684b22990f668955
robertmartinsen <robertmartinsen2000@gmail.com>
2023-11-19 11:10:29 +0100

    mybooking file added and created

--- exam-app/src/pages/MyBookings.js
+++ exam-app/src/pages/MyBookings.js
@@ -1,9 +1,21 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import classes from "./MyBookings.module.scss"
 
 function MyBookings() {
   return (
     <div className="MyBookings">
-      <h1>MyBookings</h1>
+      <div className="container pt-5 col-md-8">
+        <h1 className="text-center">
+          Oops... Seems like you are not signed in
+        </h1>
+        <h2 className="text-center pt-4">Sign in to see your bookings!</h2>
+        <div className={`pt-4 d-flex justify-content-center`}>
+          <NavLink className={classes.signInBtn} to="/Login">
+            Click Here
+          </NavLink>
+        </div>
+      </div>
     </div>
   )
 }
